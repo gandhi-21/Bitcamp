605282444e3bcfe5bf2c999c305b077efd1b1447
Gaurav Gandhi <Gaurav.Gandhi@stonybrook.edu>
2018-04-07 02:28:15 -0400

    "Updated the scraper to include content of the Author and his summary"

--- Author.py
+++ Author.py
@@ -2,9 +2,18 @@ import wikipedia
 
 Author = input("Enter Author's Name")
 
+pg = wikipedia.page(Author)
+
+text_file1 = open("Content.txt","w")
+text_file1.write(pg.content)
+text_file1.close()
+
 text_file = open("Output.txt", "w")
 text_file.write(Author)
 text_file.close()
 
+text_file2 = open("Author History.txt","w")
+text_file2.write(wikipedia.summary(Author))
+text_file2.close
 
 print(wikipedia.summary(Author))
